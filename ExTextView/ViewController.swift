7f098e54403314a7bfc9f4ba35023b8b7ecfe884
jake <palatable7@naver.com>
2022-01-30 03:19:33 +0900

    Apply placeholder UITextView

--- ExTextView/ViewController.swift
+++ ExTextView/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import Then
+import SnapKit
+import UITextView_Placeholder
 
 class ViewController: UIViewController {
-
+  // Font
+  private let myFont = UIFont.systemFont(ofSize: 16)
+  private lazy var attributedString: NSMutableAttributedString = {
+    let placeholderText = "abcd@google.com"
+    let range = (placeholderText as NSString).range(of: placeholderText)
+    let attributedString = NSMutableAttributedString(string: placeholderText)
+    let attributes: [NSAttributedString.Key: Any] = [
+      .font: self.myFont
+    ]
+    attributedString.addAttributes(attributes, range: range)
+    return attributedString
+  }()
+  
+  // MARK: UI
+  private lazy var textView = UITextView().then {
+    // 주의: $0.attributedString으로 설정하면 attributedPlaceholder 속성과 겹쳐서 적용 안되므로,
+    // attributedString사용하지 않고 font, textColor로 설정할 것
+    
+    // font
+    $0.attributedPlaceholder = self.attributedString
+    $0.font = self.myFont
+    $0.textColor = UIColor.black
+    
+    // Inset
+    $0.textContainerInset = .zero
+    $0.contentInset = .zero
+    $0.scrollIndicatorInsets = .zero
+    
+    // layer
+    $0.layer.borderWidth = 1
+    $0.layer.borderColor = UIColor.gray.cgColor
+    
+    // etc.
+    $0.returnKeyType = .done
+    $0.isEditable = true
+    $0.isScrollEnabled = true
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    self.view.addSubview(self.textView)
+    self.textView.snp.makeConstraints {
+      $0.top.left.right.equalToSuperview().inset(100)
+      $0.height.equalTo(130)
+    }
   }
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ExTextView' do
+  use_frameworks!
+  pod 'UITextView+Placeholder'
+  pod 'SnapKit'
+  pod 'Then'
+end
